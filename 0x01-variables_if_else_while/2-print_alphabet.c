12e239f8f0bdf65905a6cf4b1724dc94819f056d
radjifatim <fatimradji1@gmail.com>
2021-08-26 23:48:29 -0700

    use

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int l_alpha;
+
+	for (l_alpha = 97; l_alpha <= 122; l_alpha++)
+	{
+	putchar(l_alpha);
+	}
+	putchar(10);
+	return (0);
+}
