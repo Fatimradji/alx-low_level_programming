680bdce8f31fe661f3074a5b5bb8702b236680ee
radjifatim <fatimradji1@gmail.com>
2021-08-26 23:53:42 -0700

    use

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+	putchar(num);
+	}
+	putchar(10);
+	return (0);
