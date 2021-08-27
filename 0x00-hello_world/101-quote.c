af699a1fc00556042d31b402d0a5f1af24e9d862
radjifatim <fatimradji1@gmail.com>
2021-08-26 19:34:10 -0700

    use  it

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - executes all your body of functions and classes
+ *
+ * Return: Always success return 0, but 1 if prompt with error
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
