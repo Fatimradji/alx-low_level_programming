ceea5dd1dfc058b6623186f59f0e00f9b7c8b8d7
radjifatim <fatimradji1@gmail.com>
2021-09-01 00:42:46 -0700

    use

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,13 @@
-#include "main.h"
-
-/**
- * main - prints Holberton, followed by a new line.
- * Return: Always 0 (success)
- */
-
-int main(void)
-{
-	char *c = "_putchar";
-	int a;
-
-	for (a = 0; a < 8; a++)
-		_putchar(c[a]);
-	_putchar('\n');
-	return (0)
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
