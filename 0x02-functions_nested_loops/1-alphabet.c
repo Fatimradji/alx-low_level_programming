c71127fcfbc98034fecc73bd093df6bfd98ccf0b
radjifatim <fatimradji1@gmail.com>
2021-09-01 01:11:12 -0700

    use it

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the english alphabet from a to z.
+ *
+ * Return: nothing.
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(c + i);
+	}
+	_putchar(10);
+}
