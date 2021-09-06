c2b3138a55a654565b0b4edebaebafb7652dd61b
radjifatim <fatimradji1@gmail.com>
2021-09-06 12:54:16 -0700

    use my codes

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,20 @@
 #include "main.h"
-#include <stdlib.h>
-char *_strcpy(char *dest, char *src)
-	int len =0;
-	while (src[len] != 0)
-{
-	dest[lren] = *(src + len);
-	len++;
-}
-return (dest);
-}
 
+/**
+ * _strcpy - copy from source and paste to destination
+ * @dest: target destination for copy
+ * @src: copy from
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (src[len] != 0)
+	{
+		dest[len] = *(src + len);
+		len++;
+	}
+
+	return (dest);
+}
