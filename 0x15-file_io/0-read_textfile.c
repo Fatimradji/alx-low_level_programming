8a3c208426eb3dadc1462b6feb9a9fd676fd13dd
radjifatim <fatimradji1@gmail.com>
2021-10-22 08:22:31 -0700

    use me

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,14 +9,14 @@
 	 */
 	ssize_t read_textfile(const char *filename, size_t letters)
 	{
-		int fad;
-		int a, t;
+		int fd;
+		int s, t;
 		char *buf;
 	
 		if (!filename)
 			return (0);
 	
-		fad = open(filename, O_RDONLY);
+		fd = open(filename, O_RDONLY);
 		if (fd < 0)
 			return (0);
 	
@@ -24,17 +24,17 @@
 		if (!buf)
 			return (0);
 	
-		s = read(fad, buf, letters);
-		if (a < 0)
+		s = read(fd, buf, letters);
+		if (s < 0)
 		{
 			free(buf);
 			return (0);
 		}
-		buf[a] = '\0';
+		buf[s] = '\0';
 	
-		close(fad);
+		close(fd);
 	
-		t = write(STDOUT_FILENO, buf, a);
+		t = write(STDOUT_FILENO, buf, s);
 		if (t < 0)
 		{
 			free(buf);
