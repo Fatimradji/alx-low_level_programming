0cb251f662566763fea5c97819c25ef90766a110
radjifatim <fatimradji1@gmail.com>
2021-10-22 08:03:17 -0700

    use me

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 	int create_file(const char *filename, char *text_content)
 	{
 		int fd;
-		int t, d = 0;
+		int t, s = 0;
 	
 		if (!filename)
 			return (-1);
@@ -21,10 +21,10 @@
 	
 		if (text_content)
 		{
-			while (text_content[d])
-				d++;
-			t = write(fd, text_content, d);
-			if (t != d)
+			while (text_content[s])
+				s++;
+			t = write(fd, text_content, s);
+			if (t != s)
 				return (-1);
 		}
 	
